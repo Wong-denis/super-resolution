e4acfedb9dc98e6f1b8020cc0c092cc40ab7a7bf
Wong-denis <f92jj6be@gmail.com>
2023-03-24 15:59:39 +0800

    First Commit

---
+++ inference.py
@@ -0,0 +1,205 @@
+import argparse
+import cv2
+import glob
+import numpy as np
+import os
+import torch
+from basicsr.utils import imwrite
+import time
+
+from gfpgan import GFPGANer
+
+
+def main():
+    """Inference demo for GFPGAN (for users).
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-i',
+        '--input',
+        type=str,
+        default='inputs/whole_imgs',
+        help='Input image or folder. Default: inputs/whole_imgs')
+    parser.add_argument('-o', '--outputs', type=str, default='results', help='Output folder. Default: results')
+    # we use version to select models, which is more user-friendly
+    parser.add_argument(
+        '-v', '--version', type=str, default='1.3', help='GFPGAN model version. Option: 1 | 1.2 | 1.3. Default: 1.3')
+    parser.add_argument(
+        '-s', '--upscale', type=int, default=2, help='The final upsampling scale of the image. Default: 2')
+
+    parser.add_argument(
+        '--bg_upsampler', type=str, default='realesrgan', help='background upsampler. Default: realesrgan')
+    parser.add_argument(
+        '--bg_tile',
+        type=int,
+        default=400,
+        help='Tile size for background sampler, 0 for no tile during testing. Default: 400')
+    parser.add_argument('--suffix', type=str, default=None, help='Suffix of the restored faces')
+    parser.add_argument('--only_center_face', action='store_true', help='Only restore the center face')
+    parser.add_argument('--aligned', action='store_true', help='Input are aligned faces')
+    parser.add_argument(
+        '--ext',
+        type=str,
+        default='auto',
+        help='Image extension. Options: auto | jpg | png, auto means using the same extension as inputs. Default: auto')
+    parser.add_argument('-w', '--weight', type=float, default=0.5, help='Adjustable weights.')
+
+    ### resize ###
+    parser.add_argument('-rs','--resize', type=float, default=1, help="resize scale")
+
+    # args = parser.parse_args()
+
+    args = parser.parse_args()
+
+    # ------------------------ input & output ------------------------
+    print('=======================')
+    print(args.resize)
+    if args.input.endswith('/'):
+        args.input = args.input[:-1]
+    if os.path.isfile(args.input):
+        img_list = [args.input]
+    else:
+        img_list = sorted(glob.glob(os.path.join(args.input, '*')))
+
+    os.makedirs(args.outputs, exist_ok=True)
+
+    # ------------------------ set up background upsampler ------------------------
+    if args.bg_upsampler == 'realesrgan':
+        if not torch.cuda.is_available():  # CPU
+            import warnings
+            warnings.warn('The unoptimized RealESRGAN is slow on CPU. We do not use it. '
+                          'If you really want to use it, please modify the corresponding codes.')
+            bg_upsampler = None
+        else:
+            from basicsr.archs.rrdbnet_arch import RRDBNet
+            from realesrgan import RealESRGANer
+            model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=2)
+            bg_upsampler = RealESRGANer(
+                scale=2,
+                model_path='https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth',
+                model=model,
+                tile=args.bg_tile,
+                tile_pad=10,
+                pre_pad=0,
+                half=True)  # need to set False in CPU mode
+    else:
+        bg_upsampler = None
+
+    # ------------------------ set up GFPGAN restorer ------------------------
+    if args.version == '1':
+        arch = 'original'
+        channel_multiplier = 1
+        model_name = 'GFPGANv1'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.1.0/GFPGANv1.pth'
+    elif args.version == '1.2':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANCleanv1-NoCE-C2'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v0.2.0/GFPGANCleanv1-NoCE-C2.pth'
+    elif args.version == '1.3':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANv1.3'
+        url = 'https://github.com/TencentARC/GFPGAN/weightreleases/download/v1.3.0/GFPGANv1.3.pth'
+    elif args.version == '1.4':
+        arch = 'clean'
+        channel_multiplier = 2
+        model_name = 'GFPGANv1.4'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.4.pth'
+    elif args.version == 'RestoreFormer':
+        arch = 'RestoreFormer'
+        channel_multiplier = 2
+        model_name = 'RestoreFormer'
+        url = 'https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/RestoreFormer.pth'
+    else:
+        raise ValueError(f'Wrong model version {args.version}.')
+
+    # determine model paths
+    model_path = os.path.join('experiments/pretrained_models', model_name + '.pth')
+    if not os.path.isfile(model_path):
+        model_path = os.path.join('gfpgan/weights', model_name + '.pth')
+    if not os.path.isfile(model_path):
+        # download pre-trained models from url
+        model_path = url
+
+    restorer = GFPGANer(
+        model_path=model_path,
+        upscale=args.upscale,
+        arch=arch,
+        channel_multiplier=channel_multiplier,
+        bg_upsampler=bg_upsampler)
+
+    # ------------------------ restore ------------------------
+    runtime_read = 0
+    runtime_noread = 0
+    for img_path in img_list:
+
+        before_read_t = time.time()
+        # read image
+        img_name = os.path.basename(img_path)
+        print(f'Processing {img_name} ...')
+        basename, ext = os.path.splitext(img_name)
+        input_img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+        ### do some resize
+        input_img = cv2.resize(input_img, (160,120))
+
+        after_read_t = time.time()
+        print("The time of reading image is :",(after_read_t-before_read_t) * 10**3, "ms")
+        # restore faces and background if necessary
+        cropped_faces, restored_faces, restored_img = restorer.enhance(
+            input_img,
+            has_aligned=args.aligned,
+            only_center_face=args.only_center_face,
+            paste_back=True,
+            weight=args.weight)
+
+        # save faces
+        for idx, (cropped_face, restored_face) in enumerate(zip(cropped_faces, restored_faces)):
+            # save cropped face
+            save_crop_path = os.path.join(args.output, 'cropped_faces', f'{basename}_{idx:02d}.png')
+            imwrite(cropped_face, save_crop_path)
+            # save restored face
+            if args.suffix is not None:
+                save_face_name = f'{basename}_{idx:02d}_{args.suffix}.png'
+            else:
+                save_face_name = f'{basename}_{idx:02d}.png'
+            save_restore_path = os.path.join(args.output, 'restored_faces', save_face_name)
+            imwrite(restored_face, save_restore_path)
+            # save comparison image
+            cmp_img = np.concatenate((cropped_face, restored_face), axis=1)
+            imwrite(cmp_img, os.path.join(args.output, 'cmp', f'{basename}_{idx:02d}.png'))
+
+        after_sr_t = time.time()
+        print("The time of GFPGAN is (with reading):",(after_sr_t-before_read_t) * 10**3, "ms")
+        print("The time of GFPGAN is (without reading):",(after_sr_t-after_read_t) * 10**3, "ms")
+        runtime_read += after_sr_t-before_read_t
+        runtime_noread += after_sr_t-after_read_t
+        # save restored img
+        if restored_img is not None:
+            if args.ext == 'auto':
+                extension = ext[1:]
+            else:
+                extension = args.ext
+
+            if args.suffix is not None:
+                save_restore_path = os.path.join(args.outputs, 'restored_imgs', f'{basename}_{args.suffix}.{extension}')
+            else:
+                save_restore_path = os.path.join(args.outputs, 'restored_imgs', f'{basename}.{extension}')
+            imwrite(restored_img, save_restore_path)
+            if args.resize > 1:
+                h, w = restored_img.shape[:2]
+                resize_img = cv2.resize(restored_img, (int(w*args.resize),int(h*args.resize)), interpolation=cv2.INTER_LINEAR)
+                imwrite(resize_img, os.path.join(args.output, 'resize_imgs', f'{basename}_{args.suffix}.{extension}'))
+            elif args.resize < 1:
+                h, w = restored_img.shape[:2]
+                resize_img = cv2.resize(restored_img, (int(w*args.resize),int(h*args.resize)), interpolation=cv2.INTER_AREA)
+                imwrite(resize_img, os.path.join(args.output, 'resize_imgs', f'{basename}_{args.suffix}.{extension}'))
+
+
+    print(f'Results are in the [{args.outputs}] folder.')
+    print('runtime (with reading time): ', (runtime_read/len(img_list))* 10**3, "ms")
+    print('runtime (without reading time): ', (runtime_noread/len(img_list))* 10**3, "ms")
+
+if __name__ == '__main__':
+    main()
